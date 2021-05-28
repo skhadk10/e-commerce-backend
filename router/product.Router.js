dfa124fe1d4934515ad964c3e6ad26892c641a6e
Ricky Chettri <rickeykhd@gmail.com>
2021-05-28 16:49:16 +1000

    card duplicated fixed and qty added

--- router/product.Router.js
+++ router/product.Router.js
@@ -1,7 +1,7 @@
 import express from "express";
 import {
   getProduct,
-  getProductBySlug,
+  // getProductBySlug,
 } from "../model/product/clientProduct.model.js";
 
 const router = express.Router();
@@ -32,26 +32,26 @@ router.get("/", async (req, res) => {
     throw new Error(error.message);
   }
 });
-router.get("/:slug", async (req, res) => {
-  console.log("hello from router");
-  try {
-    const { slug } = req.params;
-    const product = await getProductBySlug(slug);
-    console.log(product);
-    if (product) {
-      return res.json({
-        status: "success",
-        message: "fetch success",
-        product,
-      });
-    }
-    res.json({
-      status: "fail",
-      message: "fetch unsuccess",
-      product,
-    });
-  } catch (error) {
-    throw new Error(error.message);
-  }
-});
+// router.get("/:slug", async (req, res) => {
+//   console.log("hello from router");
+//   try {
+//     const { slug } = req.params;
+//     const product = await getProductBySlug(slug);
+//     console.log(product);
+//     if (product) {
+//       return res.json({
+//         status: "success",
+//         message: "fetch success",
+//         product,
+//       });
+//     }
+//     res.json({
+//       status: "fail",
+//       message: "fetch unsuccess",
+//       product,
+//     });
+//   } catch (error) {
+//     throw new Error(error.message);
+//   }
+// });
 export default router;
